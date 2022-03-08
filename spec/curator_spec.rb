e85c765a8a4b043ec651f480463806ab64325d5a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-07 20:36:21 -0700

    Test: Curator can add artists

--- spec/curator_spec.rb
+++ spec/curator_spec.rb
@@ -17,6 +17,20 @@ describe Curator do
       artist_id: "2",
       year: "1941"
     })
+    @artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    })
+    @artist_2 = Artist.new({
+      id: "2",      
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
 
   end
 
@@ -30,5 +44,11 @@ describe Curator do
     @curator.add_photograph(@photo_1)
     @curator.add_photograph(@photo_2)
     expect(@curator.photographs).to eq([@photo_1, @photo_2])
+  end
+
+  it "can add artists" do
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    expect(@curator.artists).to eq([@artist_1, @artist_2])
   end
 end
