1bf724ff325db7685d76ebfe34665cb7d91483c6
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-07 20:16:32 -0700

    Create Photograph class and give it attributes

--- spec/artist_spec.rb
+++ spec/artist_spec.rb
@@ -3,20 +3,21 @@ require "./lib/photograph"
 
 describe Photograph do
   before(:each) do
-  attributes = {
-  id: "1",
-  name: "Rue Mouffetard, Paris (Boy with Bottles)",
-  artist_id: "4",
-  year: "1954"
-  })
-  photograph = Photograph.new(attributes)
+    attributes = {
+    id: "1",
+    name: "Rue Mouffetard, Paris (Boy with Bottles)",
+    artist_id: "4",
+    year: "1954"
+    }
+    @photograph = Photograph.new(attributes)
   end
 
+
   it "exists and has readable attributes" do
-    expect(photograph).to be_a(Photograph)
-    ecpect(photograph.id).to eq(1)
-    ecpect(photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
-    ecpect(photograph.artist_id).to eq("4")
-    ecpect(photograph.year).to eq("1954")
+    expect(@photograph).to be_a(Photograph)
+    expect(@photograph.id).to eq(1)
+    expect(@photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
+    expect(@photograph.artist_id).to eq("4")
+    expect(@photograph.year).to eq("1954")
   end
 end
