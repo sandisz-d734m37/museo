cddcf258a72c27918bf524e446137251c7150046
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-07 20:53:08 -0700

    Test: Curator can list photographs by artist

--- spec/curator_spec.rb
+++ spec/curator_spec.rb
@@ -58,3 +58,71 @@ describe Curator do
     expect(@curator.find_artist_by_id("1")).to eq(@artist_1)
   end
 end
+
+describe Curator do
+  before(:each) do
+    @curator = Curator.new
+    @photo_1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+    })
+    @photo_2 = Photograph.new({
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    })
+    @photo_3 = Photograph.new({
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    })
+    @photo_4 = Photograph.new({
+      id: "4",
+      name: "Monolith, The Face of Half Dome",
+      artist_id: "3",
+      year: "1927"
+    })
+    @artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    })
+    @artist_2 = Artist.new({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
+    @artist_3 = Artist.new({
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    })
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+  end
+
+  it "can list photographs by artist" do
+    expect(curator.photographs_by_artist).to eq({
+      @artist_1: [@photo_1]
+      @artist_2: [@photo_2]
+      @artist_3: [@photo_3, @photo_4]
+      })
+  end
+
+
+end
