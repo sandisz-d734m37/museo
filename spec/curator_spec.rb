455be62ae96aa3ac401c7828ec683b2a5dd67518
Sandisz Thieme <sandiszthieme@gmail.com>
2022-03-07 20:33:44 -0700

    Test: Curator can add photographs to it's collection

--- spec/curator_spec.rb
+++ spec/curator_spec.rb
@@ -17,11 +17,18 @@ describe Curator do
       artist_id: "2",
       year: "1941"
     })
+
   end
 
   it "exists and has readable attributes" do
     expect(@curator).to be_a(Curator)
     expect(@curator.photographs).to eq([])
     expect(@curator.artists).to eq([])
+  end
+
+  it "can add photographs to it's collection" do
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    expect(@curator.photographs).to eq([@photo_1, @photo_2])
   end
 end
